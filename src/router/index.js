7c2013e6aea68d86832c37db5e1c8d3b7590f0ba
emarthkopp <emarth@outlook.com>
2023-08-08 13:59:16 +0200

    Ajout de la carte -- Carte habilitation dematerialisé exemple

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/carte',
+    name: 'Carte hab',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Carte.vue')
+  },
+  {
     path: '/element/:id',
     name: 'Element',
     component: () => import(/* webpackChunkName: "about" */ '../views/Element.vue'),
