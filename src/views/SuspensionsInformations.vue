4e18677a2bb5b19d571d97dc87fb6a014ce49248
deathtroll22 <deathtroll@laposte.net>
2023-08-19 15:02:19 +0200

    indentation et commentaire jsdoc

--- src/views/SuspensionsInformations.vue
+++ src/views/SuspensionsInformations.vue
@@ -32,37 +32,6 @@
 							habilitation</p>
 					</div>
 				</div>
-
-				<!-- debogage
-
-		<div class="card text-center mt-5">
-			<div class="card-body bg-danger">
-				<h2 class="text-white">Débogage :</h2>
-
-				<div class="card mb-4">
-					<div class="card-body">
-						<h2 class="card-title">Habilitations by perso :</h2>
-						<div>{{ habilitationPersonnel }}</div>
-					</div>
-				</div>
-
-				<div class="card mb-4">
-					<div class="card-body">
-						<h2 class="card-title">Suspensions : </h2>
-						<div>{{ suspensions }}</div>
-					</div>
-				</div>
-
-				<div class="card">
-					<div class="card-body">
-						<h2 class="card-title">Types :</h2>
-						<div>{{ types }}</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	-->
-
 			</div>
 		</div>
 	</div>
@@ -72,57 +41,95 @@
 import { mapState } from 'vuex';
 
 export default {
-	computed: {
-		...mapState(['suspensions', 'personnels', 'habilitations', 'types']),
+    computed: {
 
-		personnel() {
-			return this.personnels.find(el => el.id == this.$route.params.id);
-		},
+        ...mapState(['suspensions', 'personnels', 'habilitations', 'types']),
 
-		habilitationPersonnel() {
-			return this.habilitations.filter(el => el.personnel_id == this.$route.params.id);
-		},
+        /**
+         * Retourne l'objet personnel correspondant à l'ID de l'URL.
+         * @return {Object} L'objet personnel correspondant à l'ID.
+         */
+        personnel() {
+            return this.personnels.find(el => el.id == this.$route.params.id);
+        },
 
-		getSuspensions() {
-			return this.habilitationPersonnel.reduce((suspensions, hab) => {
-				const suspension = this.suspensions.find(el => el.habilitation_id === hab.id);
-				if (suspension) {
-					suspensions.push(suspension);
-				}
-				return suspensions;
-			}, []);
-		},
+        /**
+         * Retourne les habilitations associées à ce personnel.
+         * @return {Array} Les habilitations du personnel.
+         */
+        habilitationPersonnel() {
+            return this.habilitations.filter(el => el.personnel_id == this.$route.params.id);
+        },
 
-		getNonSuspendedHabilitations() {
-			return this.habilitationPersonnel.filter(hab => !this.getSuspensions.some(sus => sus.habilitation_id === hab.id));
-		},
+        /**
+         * Retourne les suspensions associées aux habilitations du personnel.
+         * @return {Array} Les suspensions du personnel.
+         */
+        getSuspensions() {
+            return this.habilitationPersonnel.reduce((suspensions, hab) => {
+                const suspension = this.suspensions.find(el => el.habilitation_id === hab.id);
+                if (suspension) {
+                    suspensions.push(suspension);
+                }
+                return suspensions;
+            }, []);
+        },
 
-		getSuspendedHabilitations() {
-			return this.habilitationPersonnel.filter(hab => this.getSuspensions.some(sus => sus.habilitation_id === hab.id));
-		},
+        /**
+         * Retourne les habilitations non suspendues du personnel.
+         * @return {Array} Les habilitations non suspendues du personnel.
+         */
+        getNonSuspendedHabilitations() {
+            return this.habilitationPersonnel.filter(hab => !this.getSuspensions.some(sus => sus.habilitation_id === hab.id));
+        },
 
-		getHabilitationTypeName() {
-			return habilitationTypeId => {
-				const habilitationType = this.types.find(type => type.id === habilitationTypeId);
-				return habilitationType ? habilitationType.nom : '';
-			};
-		},
-		getSuspensionDate() {
-			return habilitationId => {
-				const suspension = this.suspensions.find(sus => sus.habilitation_id === habilitationId);
-				return suspension ? suspension.dd : '';
-			};
-		},
-		formatSuspensionDate() {
-			return date => {
-				if (date) {
-					const options = { year: 'numeric', month: 'long', day: 'numeric' };
-					return new Date(date).toLocaleDateString('fr-FR', options);
-				}
-				return '';
-			};
-		}
-	},
+        /**
+         * Retourne les habilitations suspendues du personnel.
+         * @return {Array} Les habilitations suspendues du personnel.
+         */
+        getSuspendedHabilitations() {
+            return this.habilitationPersonnel.filter(hab => this.getSuspensions.some(sus => sus.habilitation_id === hab.id));
+        },
+
+        /**
+         * Retourne le nom du type d'habilitation.
+         * @param {number} habilitationTypeId - L'ID du type d'habilitation.
+         * @return {string} Le nom du type d'habilitation.
+         */
+        getHabilitationTypeName() {
+            return habilitationTypeId => {
+                const habilitationType = this.types.find(type => type.id === habilitationTypeId);
+                return habilitationType ? habilitationType.nom : '';
+            };
+        },
+
+        /**
+         * Retourne la date de suspension au format localisé.
+         * @param {number} habilitationId - L'ID de l'habilitation.
+         * @return {string} La date de suspension au format localisé.
+         */
+        getSuspensionDate() {
+            return habilitationId => {
+                const suspension = this.suspensions.find(sus => sus.habilitation_id === habilitationId);
+                return suspension ? suspension.dd : '';
+            };
+        },
+
+        /**
+         * Formate la date en utilisant les options de format.
+         * @param {string} date - La date à formater.
+         * @return {string} La date formatée.
+         */
+        formatSuspensionDate() {
+            return date => {
+                if (date) {
+                    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+                    return new Date(date).toLocaleDateString('fr-FR', options);
+                }
+                return '';
+            };
+        }
+    },
 };
 </script>
 
