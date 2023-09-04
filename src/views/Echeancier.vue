0383d48c0d7aafd34e8059f7816b4d35ac3ba654
TigrouForMatic <jules.chassany@gmail.com>
2023-09-05 00:59:28 +0200

    Amelioration Echeancier

--- src/views/Echeancier.vue
+++ src/views/Echeancier.vue
@@ -37,7 +37,7 @@
             </template>
         </template>
     </div>
-    <div class="container py-2" v-else>
+    <!-- <div class="container py-2" v-else>
         
         <h1 class="fs-3 my-3">Consultation de l'échéancier</h1>
         <div class="card my-2">
@@ -58,10 +58,50 @@
                 </div>
             </div>
         </div> 
+    </div> -->
+
+    <div class="bg-white" v-else>
+        <div>
+			<img src="@/assets/Habilitations.png" alt="Pebble Dev" class="logo w-100">
+		</div>
+        <div class="card custom-app-color text-white">
+            <h1>Echéancier</h1>
+            <h3>Retrouver ici un echéancier de toutes les habilitations selon une liste du personnel et des habilitations. Vous pouvez classer, filtrer et affiner vos recherches grâce aux outils présents dans la liste de gauche!</h3>
+        </div>
     </div>
 
 
   </template>
+
+<style scoped>
+.logo {
+	max-width: 800px;
+	display:block;
+	margin: 100px auto;
+	margin-left: 20px auto;
+	margin-right: 20px auto;
+}
+.card {
+    background-color: #f7f7f7;
+    border-radius: 10px;
+    padding: 20px;
+    text-align: center;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    margin: 20px;
+    
+}
+
+.card h1 {
+    margin-bottom: 10px;
+}
+
+.card h3 {
+    margin-top: 10px;
+}
+.custom-app-color {
+    background-color: #F78C6B;
+}
+</style>
 
 <script>
 
