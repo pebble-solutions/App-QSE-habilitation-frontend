999fdbff68835302716e572756b55f12c11bdce8
TigrouForMatic <jules.chassany@gmail.com>
2023-09-10 17:11:19 +0200

    AJout de la recherche des types d'habilitation dans la fonctionnalité habilitation a programmer par types

--- src/components/menuList/ListHabilitationType.vue
+++ src/components/menuList/ListHabilitationType.vue
@@ -1,10 +1,16 @@
 <template>
+
+    <input type="text" class="form-control px-2" placeholder="Rechercher..." v-model="displaySearch">
     
-    <template v-for="hab in types" :key="hab.id">
+    <template v-for="hab in listTypeFiltred()" :key="hab.id">
         <app-menu-item :href="'/habilitationHab/' + hab.id">
             {{ hab.nom }}
         </app-menu-item>
     </template>
+
+    <div class="alert alert-info m-2" v-if="!listTypeFiltred()?.length">
+        Il n'y a pas de type d'habilitation enregistré avec cette recherche
+    </div>
 
     <div class="alert alert-info m-2" v-if="!types?.length">
         Il n'y a pas de type d'habilitation enregistré
@@ -17,9 +23,31 @@ import { mapState } from 'vuex'
 import AppMenuItem from '../pebble-ui/AppMenuItem.vue'
 
 export default {
+    data() {
+        return {
+            displaySearch: ''
+        }
+    },
     components: { AppMenuItem },
     computed: {
         ...mapState(['types'])
+    },
+
+    methods:{
+        listTypeFiltred(){
+            const typestoFiltred = this.types;
+            if (this.displaySearch == '') {
+                return typestoFiltred;
+            } else {
+                const searchInput = this.displaySearch.trim();
+                if (/^\d+$/.test(searchInput)) {
+                    return typestoFiltred.filter(item => searchInput.includes(item.id));
+                } else {
+                    const searchPattern = new RegExp(searchInput, 'i');
+                    return typestoFiltred.filter(item => item.nom?.toUpperCase().match(searchPattern));
+                }
+            }
+        }
     }
 }
 
