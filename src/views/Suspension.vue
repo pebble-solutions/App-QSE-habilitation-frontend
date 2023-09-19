38be57c046a5e89ebb2186ebd1eced7fa2a3696c
deathtroll22 <deathtroll@laposte.net>
2023-09-19 19:35:44 +0200

    suspensions ajout des infos complementairesn nom, date duree et com

--- src/views/Suspension.vue
+++ src/views/Suspension.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="container">
         <!-- Titre "Suspensions" avec la couleur de bg-custom -->
         <h1 class="text-center text-custom p-2 mb-4">Suspensions</h1>
         <!-- Liste "Suspensions en cours" -->
@@ -7,10 +7,38 @@
             <div class="card-body">
                 <h3 class="card-title text-center mb-3">Suspensions en cours</h3>
                 <div class="custom-div" v-for="suspension in getActiveSuspensions" :key="suspension.id">
-                    <div class="card card-hover d-flex justify-content-between align-items-center rounded px-3 py-2 mb-3">
-                        <span class="fw-bold">{{ getHabilitationTypeNom(suspension.habilitation_id) }} <span class="text-secondary">#{{ suspension.habilitation_id }}</span></span>
-                        <span>{{ formatDate(suspension.dd) }}</span>
-    <span>{{ formatDate(suspension.df) }}</span>
+                    <div class="card card-hover d-flex rounded px-3 py-2 mb-3">
+                        <div class="row text-center">
+                            <!-- Colonne pour le nom de l'habilitation -->
+                            <div class="col-md-6 text-md-end px-2">
+                                <span class="fw-bold">{{ getHabilitationTypeNom(suspension.habilitation_id) }}</span>
+                            </div>
+                            <!-- Colonne pour le nom du personnel -->
+                            <div class="col-md-4 text-md-start px-2">
+                                <p>{{ getPersonnelNom(suspension.habilitation_id) }}</p>
+                            </div>
+                            <!-- Colonne pour les dates, la durée et les commentaires -->
+                            <div class="col-md-12 p-2">
+                                <div class="card">
+                                    <div class="card-body p-1">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <span class="fw-bold">Dates:</span>
+                                                <p>{{ formatSuspensionText(suspension.dd, suspension.df) }}</p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <span class="fw-bold">Durée:</span>
+                                                <p>{{ getSuspensionDuration(suspension.dd, suspension.df) }} jours</p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <span class="fw-bold">Commentaire:</span>
+                                                <p>{{ suspension.commentaire }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -21,16 +49,47 @@
             <div class="card-body">
                 <h3 class="card-title text-center mb-3">Suspensions levées ou terminées</h3>
                 <div class="custom-div" v-for="suspension in getInactiveSuspensions" :key="suspension.id">
-                    <div class="card card-hover d-flex justify-content-between align-items-center rounded px-3 py-2 mb-2">
-                        <span class="fw-bold">{{ getHabilitationTypeNom(suspension.habilitation_id) }} <span class="text-secondary">#{{ suspension.habilitation_id }}</span></span>
-                        <span>{{ formatDate(suspension.dd) }}</span>
-    <span>{{ formatDate(suspension.df) }}</span>
+                    <div class="card card-hover d-flex rounded px-3 py-2 mb-3">
+                        <div class="row text-center">
+                            <!-- Colonne pour le nom de l'habilitation -->
+                            <div class="col-md-6 text-md-end px-2">
+                                <span class="fw-bold">{{ getHabilitationTypeNom(suspension.habilitation_id) }}</span>
+                            </div>
+                            <!-- Colonne pour le nom du personnel -->
+                            <div class="col-md-4 text-md-start px-2">
+                                <p>{{ getPersonnelNom(suspension.habilitation_id) }}</p>
+                            </div>
+                            <!-- Colonne pour les dates, la durée et les commentaires -->
+                            <div class="col-md-12 p-2">
+                                <div class="card">
+                                    <div class="card-body p-1">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <span class="fw-bold">Dates:</span>
+                                                <p>{{ formatSuspensionText(suspension.dd, suspension.df) }}</p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <span class="fw-bold">Durée:</span>
+                                                <p>{{ getSuspensionDuration(suspension.dd, suspension.df) }} jours</p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <span class="fw-bold">Commentaire:</span>
+                                                <p>{{ suspension.commentaire }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    
 </template>
+
+
 
 
 <script>
@@ -45,54 +104,108 @@ export default {
             },
             habilitationFromPerso: '',
             listControlDone: '',
-                   
+
         };
     },
 
     methods: {
+        /**
+      * Formate le texte pour l'affichage de la suspension.
+      * @param {string} dd - La date de début.
+      * @param {string} df - La date de fin.
+      * @return {string} Le texte formaté.
+      */
+        formatSuspensionText(dd, df) {
+            if (df === null) {
+                return 'suspendu le ' + this.formatDate(dd);
+            } else if (dd && df) {
+                const formattedDD = this.formatDate(dd);
+                const formattedDF = this.formatDate(df);
+                return `suspendu du ${formattedDD} au ${formattedDF}`;
+            }
+            return ''; // Vous pouvez ajuster ce cas selon vos besoins.
+        },
+
+        /**
+      * Retourne le nom du personnel dont l'ID correspond à habilitation.personnel.id.
+      * @param {number} habilitationId - L'ID de l'habilitation.
+      * @return {string} Le nom du personnel.
+      */
+        getPersonnelNom(habilitationId) {
+            const habilitation = this.habilitations.find(hab => hab.id === habilitationId);
+            if (habilitation && habilitation.personnel_id) {
+                const personnel = this.personnels.find(personnel => personnel.id === habilitation.personnel_id);
+                if (personnel && personnel.cache_nom) {
+                    return personnel.cache_nom;
+                }
+            }
+            return ''; // Retourne une chaîne vide si aucune correspondance n'est trouvée.
+        },
+
+        /**
+* Calcule la durée de la suspension en jours.
+* @param {string} dd - La date de début.
+* @param {string} df - La date de fin.
+* @return {number} La durée en jours.
+*/
+        getSuspensionDuration(dd, df) {
+            if (dd && df) {
+                const start = new Date(dd);
+                const end = new Date(df);
+                const timeDifference = end - start;
+                const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+                return daysDifference;
+            }
+            return 0; // Si les dates sont absentes ou incorrectes, la durée est de 0 jours.
+        },
+
 
     },
+
+
 
     computed: {
 
         ...mapState(['suspensions', 'personnels', 'habilitations', 'types',]),
 
-         /**
-     * Formate la date en utilisant les options de format.
-     * @param {string} date - La date à formater.
-     * @return {string} La date formatée en français.
-     */
-    formatDate() {
-        return date => {
-            if (date) {
-                const options = { year: 'numeric', month: 'long', day: 'numeric' };
-                return new Date(date).toLocaleDateString('fr-FR', options);
-            }
-            return '';
-        };
-    },
 
-         /**
-     * Retourne le nom du type d'habilitation en fonction de l'ID de l'habilitation.
-     * @param {number} habilitationId - L'ID de l'habilitation.
-     * @return {string} Le nom du type d'habilitation.
-     */
-    getHabilitationTypeNom() {
-        return habilitationId => {
-            const habilitation = this.habilitations.find(hab => hab.id === habilitationId);
-            if (habilitation) {
-                const type = this.types.find(type => type.id === habilitation.habilitation_type_id);
-                return type ? type.nom : '';
-            }
-            return '';
-        };
-    },
 
-       /**
-         * Retourne les suspensions en cours (date df est null ou supérieure à la date du jour).
-         * @return {Array} Les suspensions en cours.
-         */
-         getActiveSuspensions() {
+        /**
+    * Formate la date en utilisant les options de format.
+    * @param {string} date - La date à formater.
+    * @return {string} La date formatée en français.
+    */
+        formatDate() {
+            return date => {
+                if (date) {
+                    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+                    return new Date(date).toLocaleDateString('fr-FR', options);
+                }
+                return '';
+            };
+        },
+
+        /**
+    * Retourne le nom du type d'habilitation en fonction de l'ID de l'habilitation.
+    * @param {number} habilitationId - L'ID de l'habilitation.
+    * @return {string} Le nom du type d'habilitation.
+    */
+        getHabilitationTypeNom() {
+            return habilitationId => {
+                const habilitation = this.habilitations.find(hab => hab.id === habilitationId);
+                if (habilitation) {
+                    const type = this.types.find(type => type.id === habilitation.habilitation_type_id);
+                    return type ? type.nom : '';
+                }
+                return '';
+            };
+        },
+
+        /**
+          * Retourne les suspensions en cours (date df est null ou supérieure à la date du jour).
+          * @return {Array} Les suspensions en cours.
+          */
+        getActiveSuspensions() {
             const today = new Date();
             return this.suspensions.filter(suspension => !suspension.df || new Date(suspension.df) >= today);
         },
@@ -117,11 +230,10 @@ export default {
 
     },
 
-    
+
 }
 </script>
 <style scoped>
-
 .bg-custom {
     background-color: #f78c6b9a;
 }
@@ -132,12 +244,16 @@ export default {
 
 /* Effet d'agrandissement et d'ombre au survol */
 .card-hover:hover {
-    transform: translate3D(0,-1px,0) scale(1.025); /* Zoom in */
-    box-shadow: 8px 28px 50px rgba(39,44,49,.07), 1px 6px 12px rgba(39,44,49,.04);
+    transform: translate3D(0, -1px, 0) scale(1.025);
+    /* Zoom in */
+    box-shadow: 8px 28px 50px rgba(33, 37, 41, 0.217), 1px 6px 12px rgba(39, 44, 49, .04);
     transition: all .4s ease;
     cursor: pointer;
-    background-color: #fadcd1;
-}.card {
+    background-color: #fceae4;
+    border: #f78c6b75 solid 2px;
+}
+
+.card {
     border: 0;
 
 }
